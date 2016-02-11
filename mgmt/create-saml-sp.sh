acd89ce459d005554588ff628a024f6264e88bba
Irina <irina.epshteyn@ge.com>
2016-02-11 09:15:25 -0800

    Added a script for creating saml service provider and a README
    Signed-off-by: Navyatha <bondugula@ge.com>

---
+++ mgmt/create-saml-sp.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -v -x
+
+while getopts ":n:m:i" opt; do
+    case $opt in
+	n)
+	    origin_name=$OPTARG
+	    ;;
+        m)
+            saml_metadata_file=$OPTARG
+            ;;
+        i)
+            skip_ssl="true"
+            ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+if [[ -z "$origin_name" ]]; then
+    echo "You must specify the origin name with option -n."
+    exit 1
+fi
+echo $origin_name | grep ^.*[\]\^\:\ \?\/\@\#\[\{\}\!\$\&\'\(\)\*\+\,\;\=\~\`\%\|\<\>\"].*$
+#A status code of 0 means that there was a special character in the origin name.
+if [ $? == 0 ]; then
+    echo "Origin name $origin_name contains special characters. Remove the special characters and retry."
+    exit 1
+fi   
+
+if [[ -z "$saml_metadata_file" ]]; then
+    echo "You must specify the sp config file with option -m."
+    exit 1
+fi
+
+left='{"metaDataLocation":"'
+right='","spEntityId":"'"$origin_name"'","nameID":"urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified","singleSignOnServiceIndex":0,"metadataTrustCheck":false,"socketFactoryClassName":"org.apache.commons.httpclient.protocol.DefaultProtocolSocketFactory"}'
+
+
+esc_left=$(echo $left | sed 's/"/\\"/g')
+esc_right=$(echo $right | sed 's/"/\\"/g')
+
+# dos2unix for stupid OSX that doesn't have dos2unix
+esc_middle_0=$(cat "$saml_metadata_file" | col -b)
+# Replaces all newlines with \\n
+esc_middle_1=$(echo "$esc_middle_0" | awk '$1=$1' ORS='\\\\n')
+# Replaces all quotes with \\\"
+esc_middle_2=$(echo "$esc_middle_1" | sed 's/"/\\\\\\"/g')
+
+config="$esc_left$esc_middle_2$esc_right"
+
+data='{"entityId":"'"$origin_name"'","name":"'"$origin_name"'","config":"'"$config"'","active":true}'
+
+if [[ -z $skip_ssl ]]; then
+    uaac curl -XPOST -H"Accept:application/json" -H"Content-Type:application/json" /saml/service-providers -d "$data"
+else
+    uaac curl -XPOST -H"Accept:application/json" -H"Content-Type:application/json" /saml/service-providers -d "$data" --insecure
+fi
